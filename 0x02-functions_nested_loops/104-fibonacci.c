21f34cac36adcb927ee8e2322477debf573c2934
Alnazeer Hassan <alnzeer0996846568@gmail.com>
2022-10-19 14:30:35 -0700

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,53 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 
 /**
-  * print_times_table - Prints a multiplication table up to param
-  * @n: The number to be treated
-  *
-  * Return: Number matrix
-  */
-void print_times_table(int n)
+ *  * main - Prints the first 98 Fibonacci numbers, starting with
+ *   *        1 and 2, separated by a comma followed by a space.
+ *    *
+ *     * Return: Always 0.
+*/
+int main(void)
 {
-	int x, y, z;
-
-	if (n >= 0 && n <= 14)
-	{
-		for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
+{
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 = sum;
 }
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+for (count = 93; count < 99; count++)
+{
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
+return (0);
+}
+
