a49e78a647c048add2ca9487fbaf804fb11ff106
Alnazeer Hassan <alnzeer0996846568@gmail.com>
2022-10-24 10:43:17 -0700

    fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * * void prototypes()
- * *char prototypes()
- * * int prototypes()
- * */
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
 void reset_to_98(int *n);
 void _puts(char *str);
 void print_rev(char *s);
