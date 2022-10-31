4d13efc357b2fa13904b497d4eb0e31acad5c161
Alnazeer Hassan <alnzeer0996846568@gmail.com>
2022-10-31 11:31:33 -0700

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,44 @@
+
+Sign up
+dennisnderitu254
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x07-pointers_arrays_strings/0-memset.c
+@dennisnderitu254
+dennisnderitu254 function that fills memory with a constant byte
+ 1 contributor
+26 lines (23 sloc)  380 Bytes
+/*
+ * File: 0-memset.c
+ * Auth: not
+ */
+
 #include "main.h"
 
 /**
- *_memset - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @b: constant
- * @n: max bytes to use
- * Return: s
- */
+*_memset - The _memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; n > 0; i++, n--)
+	while (n)
 	{
-		s[i] = b;
+		s[n - 1] = b;
+		n--;
 	}
-
 	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,24 +1,43 @@
+
+Sign up
+dennisnderitu254
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+alx-low_level_programming/0x07-pointers_arrays_strings/main.h
+@dennisnderitu254
+dennisnderitu254 Adding main file, project Header file and _putchar function
+ 1 contributor
+25 lines (21 sloc)  656 Bytes
 #ifndef MAIN_H
 #define MAIN_H
 
 /**
- * File: holberton.h
- * Auth: Firdaus
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
+ * File: main.h
+ * Auth: Dev Nderitu
+ *description: Header file containing prototypes for all functions
+ * used in the0x07-pointers_arrays_strings directory
  */
 
+
+#include <stdio.h>
 #include <stddef.h>
 
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
+unsigned int _strspn(char *s, char *accept);
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+#endif /* MAIN_H */
